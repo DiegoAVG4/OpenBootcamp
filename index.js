db7f72b56460a4b5bb509c1da62521f79aa34fbe
DiegoAVG4 <diegovalderramag@gmail.com>
2022-08-26 22:36:44 -0500

    Ejercicio02

---
+++ index.js
@@ -0,0 +1,12 @@
+const fechaa = new Date(2003, 04, 14);
+const librofec = new Date(1967, 04,);
+let librou = ["100 años de soledad", "Gabriel Garcia Marquez", librofec];
+
+const datos = {
+    nombre: "DiegoAVG",
+    edad: 19,
+    desarrollador: true,
+    fecha: fechaa,
+    libro: librou
+}
+console.log(datos)
